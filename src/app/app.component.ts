614196903b3d0c09e738b52d035bbe685fe5ea7c
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-03-15 00:20:09 +0000

    routing: added test routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { PlaceholderHomePageComponent } from './placeholder-home-page/placeholder-home-page.component';
-
+import { MatButton } from '@angular/material/button'; // Add this import statement
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlaceholderHomePageComponent],
+  imports: [
+    RouterOutlet,
+    PlaceholderHomePageComponent,
+    MatButton,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./placeholder-home-page/placeholder-home-page.component').then(
+        (m) => m.PlaceholderHomePageComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
