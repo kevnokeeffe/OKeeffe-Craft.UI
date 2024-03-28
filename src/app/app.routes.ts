78abd671024d7c5084f46cb0ab193045f67acade
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-03-28 17:30:38 +0000

    refactor: changed routing '/' to point to home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,9 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () =>
-      import('./home/home.component').then((m) => m.HomeComponent),
-  },
-  {
     path: '',
     loadComponent: () =>
-      import('./layout/side-nav/side-nav.component').then(
-        (m) => m.SideNavComponent
-      ),
+      import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: '**',
