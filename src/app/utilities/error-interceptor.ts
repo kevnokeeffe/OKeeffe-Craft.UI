58f5c475487b425019a2318fc1a6114c210b8a36
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-04-01 01:19:38 +0100

    fix: added new errors to watch for

--- src/app/utilities/error-interceptor.ts
+++ src/app/utilities/error-interceptor.ts
@@ -21,6 +21,7 @@ export function errorInterceptor(
         error.status === 400 ||
         error.status === 403 ||
         error.status === 404 ||
+        error.status === 405 ||
         error.status === 500 ||
         error.status === 502 ||
         error.status === 503 ||
